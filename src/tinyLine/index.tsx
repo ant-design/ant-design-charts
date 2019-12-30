2b34a803a2f4308e3c5bd3201949ddbeba2eb6bd
liufu.lf <liufu.lf@antfin.com>
2019-12-30 16:24:04 +0800

    晚上Demo、修复BUG

--- src/tinyLine/index.tsx
+++ src/tinyLine/index.tsx
@@ -1,31 +1,31 @@
-// import React, { useEffect, useContext } from 'react';
-// import { TinyLine, TinyLineConfig as G2plotProps } from '@antv/g2plot';
-// import useChart from '../hooks/useChart';
-// import { ErrorBoundary, ConfigContext } from '../base';
+import React, { useEffect, useContext } from 'react';
+import { TinyLine, TinyLineConfig as G2plotProps } from '@antv/g2plot';
+import useChart from '../hooks/useChart';
+import { ErrorBoundary, ConfigContext } from '../base';
 
-// export interface TinyLineConfig extends G2plotProps {
-//   chartRef?: React.MutableRefObject<TinyLine | undefined>;
-// }
+export interface TinyLineConfig extends G2plotProps {
+  chartRef?: React.MutableRefObject<TinyLine | undefined>;
+}
 
-// const TechTinyLine: React.FC<TinyLineConfig> = (props: TinyLineConfig) => {
-//   const { chartRef, ...rest } = props;
+const TechTinyLine: React.FC<TinyLineConfig> = (props: TinyLineConfig) => {
+  const { chartRef, ...rest } = props;
 
-//   const { chart, container } = useChart<TinyLine, TinyLineConfig>(TinyLine, rest);
+  const { chart, container } = useChart<TinyLine, TinyLineConfig>(TinyLine, rest);
 
-//   useEffect(() => {
-//     if (chartRef) {
-//       chartRef.current = chart.current;
-//     }
-//   }, [chart.current]);
+  useEffect(() => {
+    if (chartRef) {
+      chartRef.current = chart.current;
+    }
+  }, [chart.current]);
 
-//   return <div ref={container} />;
-// };
+  return <div ref={container} />;
+};
 
-// export default (props: TinyLineConfig) => {
-//   const config = useContext(ConfigContext);
-//   return (
-//     <ErrorBoundary>
-//       <TechTinyLine {...config} {...props} />
-//     </ErrorBoundary>
-//   );
-// };
+export default (props: TinyLineConfig) => {
+  const config = useContext(ConfigContext);
+  return (
+    <ErrorBoundary>
+      <TechTinyLine {...config} {...props} />
+    </ErrorBoundary>
+  );
+};
