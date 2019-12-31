cb993d052d8458df7c44e06ecd6177074346b399
愚道 <tingzhao.ytz@antfin.com>
2019-12-31 14:39:45 +0800

    chore: remove Base temply

--- src/Base/index.tsx
+++
@@ -1,6 +0,0 @@
-import { ConfigContextProps, ConfigContext } from './context';
-import ErrorBoundary from './errorBoundary';
-
-const ConfigProvider = ConfigContext.Provider;
-
-export { ConfigContext, ErrorBoundary, ConfigContextProps, ConfigProvider };
