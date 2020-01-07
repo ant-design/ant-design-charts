9c0eaaa7918d9f3fefeab66c7f2391685932e0ab
liufu.lf <liufu.lf@antfin.com>
2020-01-07 20:45:57 +0800

    添加自定义节点教程

--- src/util/createNode.ts
+++ src/util/createNode.ts
@@ -1,5 +1,5 @@
-import React from '@alipay/bigfish/react';
-import ReactDOM from '@alipay/bigfish/react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 export const createNode = (children: React.ReactElement) => {
   const mountPoint = document.createElement('div');
