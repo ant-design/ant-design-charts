934e3cea449cbb321b73a433887052040c7bba08
liufu.lf <liufu.lf@antfin.com>
2020-02-19 22:54:56 +0800

    新增图表类型

---
+++ src/wordCloud/index.tsx
@@ -0,0 +1,33 @@
+import React, { useContext, useEffect } from 'react';
+import { WordCloud, WordCloudConfig as G2plotProps } from '@antv/g2plot';
+import useChart from '../hooks/useChart';
+import { ConfigContext, ErrorBoundary } from '../base';
+
+export interface WordCloudConfig extends G2plotProps {
+  chartRef?: React.MutableRefObject<WordCloud | undefined>;
+  style?: React.CSSProperties;
+  className?: string;
+}
+
+const TechWordCloud: React.FC<WordCloudConfig> = (props: WordCloudConfig) => {
+  const { chartRef, style = {}, className, ...rest } = props;
+
+  const { chart, container } = useChart<WordCloud, WordCloudConfig>(WordCloud, rest);
+
+  useEffect(() => {
+    if (chartRef) {
+      chartRef.current = chart.current;
+    }
+  }, [chart.current]);
+
+  return <div className={className} style={style} ref={container} />;
+};
+
+export default (props: WordCloudConfig) => {
+  const config = useContext(ConfigContext);
+  return (
+    <ErrorBoundary>
+      <TechWordCloud {...config} {...props} />
+    </ErrorBoundary>
+  );
+};
