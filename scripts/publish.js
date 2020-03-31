703f6dd2de072e144f5a3a791bd946e01bb606a3
kn9117 <1175863618@qq.com>
2020-03-31 17:58:42 +0800

    publish version:1.0.0

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,15 +1,15 @@
 const shell = require('shelljs');
 const path = require('path');
 
-publishToTnpm();
+publishToNpm();
 
-function publishToTnpm() {
+function publishToNpm() {
   // eslint-disable-next-line global-require
   const { version } = require(path.join('../', 'package.json'));
   if (version.includes('-rc.') || version.includes('-beta.') || version.includes('-alpha.')) {
-    shell.exec('tnpm publish --tag next');
+    shell.exec('npm publish --tag next');
   } else {
-    shell.exec('tnpm publish');
+    shell.exec('npm publish');
   }
   shell.exec('git add .');
   shell.exec(`git commit -m 'publish version:${version}'`);
