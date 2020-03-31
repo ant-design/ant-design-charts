95edc5478620830a4acab9db3712197138aed29b
kn9117 <1175863618@qq.com>
2020-03-31 18:08:35 +0800

    modify version

--- scripts/publish.js
+++ scripts/publish.js
@@ -7,9 +7,9 @@ function publishToNpm() {
   // eslint-disable-next-line global-require
   const { version } = require(path.join('../', 'package.json'));
   if (version.includes('-rc.') || version.includes('-beta.') || version.includes('-alpha.')) {
-    shell.exec('npm publish --tag next');
+    shell.exec('npm publish --access public --tag next');
   } else {
-    shell.exec('npm publish');
+    shell.exec('npm publish --access public');
   }
   shell.exec('git add .');
   shell.exec(`git commit -m 'publish version:${version}'`);
