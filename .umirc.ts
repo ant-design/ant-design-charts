97f44e7d5c4c2f725134843bd7b7b6f4d3045fb3
lxfu <1175863618@qq.com>
2020-04-14 20:00:22 +0800

    public path

--- .umirc.ts
+++ .umirc.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'antd/charts',
-  base: '/ant-design-charts',
+  base: '/ant-design-charts/',
+  publicPath: '/ant-design-charts/',
   // more config: https://d.umijs.org/config
 });
