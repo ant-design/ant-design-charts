34a5f590ff3adf25feb85f3cad3257dda06e99d5
lxfu1 <954055752@qq.com>
2020-05-08 20:40:29 +0800

    修改publicPath

--- .umirc.ts
+++ .umirc.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'antd/charts',
-  base: '/ant-design-charts/',
-  publicPath: '/ant-design-charts/',
+  base: '/',
+  publicPath: '/',
   // more config: https://d.umijs.org/config
 });
