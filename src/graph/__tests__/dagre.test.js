96157733b4322482f001ec1fe9b2e6fb987bbb56
Yanyan-Wang <yanyanwang93@gmail.com>
2020-07-09 17:22:16 +0800

    test: update the test for graph components.

--- src/graph/__tests__/dagre.test.js
+++ src/graph/__tests__/dagre.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import DagreGraph from '../';
+import { DagreGraph } from '../';
 
 describe('Dagre Graph', () => {
   const data = {
--- src/graph/__tests__/indented.test.js
+++ src/graph/__tests__/indented.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import IndentedTree from '../';
+import { IndentedTree } from '../';
 
 describe('Indented Tree Graph', () => {
   const data = {
