b2eddf0b11f5c341a747191b5b62b51265995f8c
lxfu1 <954055752@qq.com>
2020-08-27 22:13:06 +0800

    feat: 提取容器 props 类型定义

--- src/bar/index.tsx
+++ src/bar/index.tsx
@@ -1,16 +1,11 @@
 import React, { useEffect, useImperativeHandle, forwardRef } from 'react';
 import { Bar as G2plotBar, BarOptions as G2plotProps } from 'g2plot-v2';
-import useChart from '../hooks/useChart-v2';
+import useChart, { ContainerProps } from '../hooks/useChart-v2';
 import { ErrorBoundary } from '../base';
 import ChartLoading from '../util/createLoading';
 
-export interface BarConfig extends G2plotProps {
+export interface BarConfig extends G2plotProps, ContainerProps {
   chartRef?: React.MutableRefObject<G2plotBar | undefined>;
-  style?: React.CSSProperties;
-  className?: string;
-  loading?: boolean;
-  loadingTemplate?: React.ReactElement;
-  errorTemplate?: (e: Error) => React.ReactNode;
 }
 
 const BarChart = forwardRef((props: BarConfig, ref) => {
