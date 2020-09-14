9dd3da8c1836fa5c27d90bb78f1d4cde4f851782
PeachScript <scdzwyxst@gmail.com>
2020-09-15 01:01:21 +0800

    docs: add style for api tabs in doc site

--- .dumi/theme/layout.tsx
+++ .dumi/theme/layout.tsx
@@ -3,6 +3,8 @@ import { IRouteComponentProps } from 'dumi';
 import { Link, context } from 'dumi/theme';
 import Layout from 'dumi-theme-default/src/layout';
 
+import './layout.less';
+
 const importer = require.context('../../docs/.g2-plot-api', false, /\.md$/);
 const docs = importer
   .keys()
@@ -15,16 +17,23 @@ const docs = importer
 export default ({ children, ...props }: IRouteComponentProps) => {
   const { meta } = useContext(context);
   const name = meta.filePath?.match(/([\w-]+)\.md$/i)?.[1] || '';
-  const isShowApi = (props.location.query as any).api !== undefined && docs[name];
+  const isShowApi = (props.location.query as any).type === 'api' && docs[name];
 
   return (
     <Layout {...props}>
       <>
         {docs[name] && (
           <div>
-            <Link to={props.location.pathname}>Demo</Link>
-            &nbsp;|&nbsp;
-            <Link to={`${props.location.pathname}?api`}>API</Link>
+            <Link
+              className="__antd-charts-theme-tab-item"
+              to={props.location.pathname}
+              data-active={!isShowApi || undefined}
+            >Demo</Link>
+            <Link
+              className="__antd-charts-theme-tab-item"
+              to={`${props.location.pathname}?type=api`}
+              data-active={isShowApi || undefined}
+            >API</Link>
           </div>
         )}
         {isShowApi ? importer(docs[name]).default() : children}
