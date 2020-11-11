d1a6ef1068f5f41d1980fdcb79b0ddfa2282e2bc
lxfu1 <954055752@qq.com>
2020-11-11 09:47:00 +0800

    fix: 修复umd包体积问题

--- src/index.charts.ts
+++ src/index.charts.ts
@@ -1,4 +1,32 @@
-// Charts
+// 从 index import 会有包体积问题
+import Area from './area';
+import Bar from './bar';
+import Bullet from './bullet';
+import Column from './column';
+import Funnel from './funnel';
+import Histogram from './histogram';
+import Line from './line';
+import Box from './box';
+import Liquid from './liquid';
+import Heatmap from './heatmap';
+import Pie from './pie';
+import Gauge from './gauge';
+import Progress from './progress';
+import Radar from './radar';
+import RingProgress from './ringProgress';
+import Rose from './rose';
+import Scatter from './scatter';
+import TinyArea from './tinyArea';
+import TinyColumn from './tinyColumn';
+import DualAxes from './dualAxes';
+import TinyLine from './tinyLine';
+import Waterfall from './waterfall';
+import WordCloud from './wordCloud';
+import Sunburst from './sunburst';
+import Stock from './stock';
+import RadialBar from './radialBar';
+import { G2 } from '@antv/g2plot';
+
 export {
   Area,
   Bar,
@@ -27,4 +55,4 @@ export {
   RadialBar,
   Gauge,
   G2,
-} from './index';
+};
