5b69dbc37b106b6cd7e8fe1341e00bcd4fe5a2a9
lxfu1 <954055752@qq.com>
2021-06-15 11:50:56 +0800

    fix: types

--- src/facet/index.tsx
+++ src/facet/index.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useImperativeHandle, forwardRef } from 'react';
-import { Facet as G2plotFacet, FacetOptions as G2plotProps } from '@antv/g2plot';
-import useChart, { ContainerProps } from '../hooks/useChart';
+import { Facet as G2plotFacet, FacetOptions as G2plotConfig } from '@antv/g2plot';
+import useChart, { ContainerConfig } from '../hooks/useChart';
 import { getChart } from '../util';
 import { ChartRefOptions } from '../interface';
 import { ErrorBoundary } from '../base';
 import ChartLoading from '../util/createLoading';
 
-export interface FacetConfig extends G2plotProps, ContainerProps {
+export interface FacetConfig extends G2plotConfig, ContainerConfig {
   chartRef?: ChartRefOptions;
 }
 
--- src/violin/index.tsx
+++ src/violin/index.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useImperativeHandle, forwardRef } from 'react';
-import { Violin as G2plotViolin, ViolinOptions as G2plotProps } from '@antv/g2plot';
-import useChart, { ContainerProps } from '../hooks/useChart';
+import { Violin as G2plotViolin, ViolinOptions as G2plotConfig } from '@antv/g2plot';
+import useChart, { ContainerConfig } from '../hooks/useChart';
 import { getChart } from '../util';
 import { ChartRefOptions } from '../interface';
 import { ErrorBoundary } from '../base';
 import ChartLoading from '../util/createLoading';
 
-export interface ViolinConfig extends G2plotProps, ContainerProps {
+export interface ViolinConfig extends G2plotConfig, ContainerConfig {
   chartRef?: ChartRefOptions;
 }
 
