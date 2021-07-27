baeece9a378c6b2c757cfd71586dcdaf9919161c
lxfu1 <954055752@qq.com>
2021-07-27 18:53:05 +0800

    fix: types

--- src/graphs/flowAnalysisGraph/index.tsx
+++ src/graphs/flowAnalysisGraph/index.tsx
@@ -21,10 +21,29 @@ import {
   defaultNodeStyle,
 } from '../constants';
 import { registerIndicatorCardNode } from './customItem';
-import { CommonConfig, EdgeConfig, NodeConfig, CardNodeCfg, FlowGraphDatum } from '../interface';
+import {
+  CommonConfig,
+  EdgeConfig,
+  NodeConfig,
+  CardNodeCfg,
+  FlowGraphEdgeData,
+  NodeData,
+  CardItems,
+} from '../interface';
+
+export interface FlowAnalysisNodeData
+  extends NodeData<
+    Array<{
+      title?: string;
+      items?: CardItems;
+    }>
+  > {}
 
 export interface FlowAnalysisGraphConfig extends Omit<CommonConfig, 'data'> {
-  data: FlowGraphDatum;
+  data: {
+    nodes: FlowAnalysisNodeData[];
+    edges: FlowGraphEdgeData[];
+  };
 }
 
 const graphs: any = {};
@@ -175,7 +194,7 @@ const FlowAnalysisGraph: React.FC<FlowAnalysisGraphConfig> = (props) => {
     processMinimap(minimapCfg, graph);
     bindStateEvents(graph, uProps as FlowAnalysisGraphConfig);
     if (markerCfg) {
-      bindSourceMapCollapseEvents(graph, data as FlowGraphDatum);
+      bindSourceMapCollapseEvents(graph, data as FlowAnalysisGraphConfig['data']);
     }
     renderGraph(graph, data, autoFit, adjustLayout);
     if (onReady) {
