2a9dc3265343ee9746a1bfcb8d7c5125505b452d
Joel Alan <31396322+lxfu1@users.noreply.github.com>
2021-09-03 18:56:05 +0800

    fix: 修改路径 (#852)

--- scripts/chart.js
+++ scripts/chart.js
@@ -1,17 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 const ejs = require('ejs');
-const { chartNames } = require('./constants.js');
-const { lowerCase } = require('./util.js');
-const templateChartPath = path.join(__dirname, '../template/chart/index.ejs');
+const { chartNames } = require('./constants');
+const { lowerCase, checkDirExist } = require('./util.js');
+const templateChart = path.join(__dirname, '../template/chart/index.ejs');
 
 // 生成路径
-const WRITE_PATH = path.join(__dirname, '../src');
+const WRITE_PATH = path.join(__dirname, '../src/plots');
+const name = process.argv.splice(2)?.[0];
 
-try {
-  Object.keys(chartNames).forEach((chartName) => {
+const render = (chartName) => {
+  try {
     ejs.renderFile(
-      templateChartPath,
+      templateChart,
       {
         chartName,
       },
@@ -20,18 +21,28 @@ try {
           console.log('模版文件读取失败： ', err);
           return;
         }
+        const writerPath = path.join(WRITE_PATH, lowerCase(chartName));
+        checkDirExist(writerPath);
         // 生成文件内容
-        fs.writeFile(path.join(WRITE_PATH, lowerCase(chartName), 'index.tsx'), data, (err) => {
+        fs.writeFile(path.join(writerPath, 'index.tsx'), data, (err) => {
           if (err) {
             console.log(err);
+          } else {
+            console.log(`${chartName} 文件生成完成`);
           }
         });
       },
     );
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log(err);
+  }
+};
+
+if (name) {
+  render(name);
+} else {
+  Object.keys(chartNames).forEach((chartName) => {
+    render(chartName);
   });
-  // eslint-disable-next-line no-console
-  console.log('chart文件生成完成');
-} catch (err) {
-  // eslint-disable-next-line no-console
-  console.log(err);
 }
