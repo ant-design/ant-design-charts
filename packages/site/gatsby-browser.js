6ceadef44cf61db4d49021918996a07b0fcf497d
lxfu1 <954055752@qq.com>
2021-11-17 16:28:30 +0800

    chore: 临时去掉 dist

--- packages/site/gatsby-browser.js
+++ packages/site/gatsby-browser.js
@@ -4,5 +4,4 @@ window.util = require('@antv/util');
 window.react = require('react');
 window.reactDom = require('react-dom');
 window.charts = require('@ant-design/charts');
-// window.charts = require('../charts/src/index.ts');
 // window.charts = require('../flowchart/src/index')
