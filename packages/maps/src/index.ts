00dbf6dbeee48c048ee8a6c4a7c32d431ce9f6e5
lviser <yunji.me@outlook.com>
2021-11-20 14:49:04 +0800

    refactor: rename to hexbin (#976)

--- packages/maps/src/index.ts
+++ packages/maps/src/index.ts
@@ -14,6 +14,6 @@ export { ContainerConfig as MapContainerConfig, PlotRef } from './types';
 export { default as DotMap, DotMapConfig } from './components/DotMap';
 export { default as HeatMap, HeatMapConfig } from './components/HeatMap';
 export { default as GridMap, GridMapConfig } from './components/GridMap';
-export { default as HexagonMap, HexagonMapConfig } from './components/HexagonMap';
+export { default as HexbinMap, HexbinMapConfig } from './components/HexbinMap';
 export { default as AreaMap, AreaMapConfig } from './components/AreaMap';
 export { default as ChoroplethMap, ChoroplethMapConfig } from './components/ChoroplethMap';
