3da57a38ab4c8b52572ae609a28424be3d3d287d
lviser <yunji.me@outlook.com>
2021-12-01 11:11:00 +0800

    feat: path & flow map (#1004)

--- packages/maps/src/index.ts
+++ packages/maps/src/index.ts
@@ -15,5 +15,7 @@ export { default as DotMap, DotMapConfig } from './components/DotMap';
 export { default as HeatMap, HeatMapConfig } from './components/HeatMap';
 export { default as GridMap, GridMapConfig } from './components/GridMap';
 export { default as HexbinMap, HexbinMapConfig } from './components/HexbinMap';
+export { default as PathMap, PathMapConfig } from './components/PathMap';
+export { default as FlowMap, FlowMapConfig } from './components/FlowMap';
 export { default as AreaMap, AreaMapConfig } from './components/AreaMap';
 export { default as ChoroplethMap, ChoroplethMapConfig } from './components/ChoroplethMap';
