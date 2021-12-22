35f54e9db05af3b006c4d4a40ee003447151f550
lxfu1 <954055752@qq.com>
2021-12-22 20:39:10 +0800

    feat: 导出流程图原子组件

--- packages/flowchart/src/components/editor-panel/control-map-service/components/fields/index.ts
+++ packages/flowchart/src/components/editor-panel/control-map-service/components/fields/index.ts
@@ -5,4 +5,13 @@ import Size from './size';
 import Position from './position';
 import SelectField from './select';
 
+export const EditorPanels: { [key: string]: React.FC } = {
+  InputFiled,
+  ColorPicker,
+  InputNumberFiled,
+  Size,
+  Position,
+  SelectField,
+};
+
 export { InputFiled, ColorPicker, InputNumberFiled, Size, Position, SelectField };
