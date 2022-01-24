6bbfbd832d2427e9500934f0ec3a9649bdae5893
promise6512 <2523863783@qq.com>
2022-01-24 16:02:13 +0800

    style: 将registerIcon 函数拆分到 utils 目录下

--- packages/flowchart/src/components/toolbar/useToolbarConfig.ts
+++ packages/flowchart/src/components/toolbar/useToolbarConfig.ts
@@ -1,34 +1,9 @@
-import { createToolbarConfig, IconStore } from '@antv/xflow';
+import { createToolbarConfig } from '@antv/xflow';
 import { getProps, Log } from '../../util';
-import {
-  UngroupOutlined,
-  SaveOutlined,
-  GroupOutlined,
-  GatewayOutlined,
-  UndoOutlined,
-  RedoOutlined,
-  VerticalAlignTopOutlined,
-  VerticalAlignBottomOutlined,
-  CopyOutlined,
-  SnippetsOutlined,
-} from '@ant-design/icons';
 import { CommandPool, defaultCommands } from './constants';
 import { CommandItem, FlowchartProps } from '../../interface';
 import { NSToolbarConfig } from './toolbarConfig';
-
-/** 注册icon 类型 */
-const registerIcon = () => {
-  IconStore.set('SaveOutlined', SaveOutlined);
-  IconStore.set('UndoOutlined', UndoOutlined);
-  IconStore.set('RedoOutlined', RedoOutlined);
-  IconStore.set('VerticalAlignTopOutlined', VerticalAlignTopOutlined);
-  IconStore.set('VerticalAlignBottomOutlined', VerticalAlignBottomOutlined);
-  IconStore.set('GatewayOutlined', GatewayOutlined);
-  IconStore.set('GroupOutlined', GroupOutlined);
-  IconStore.set('UngroupOutlined', UngroupOutlined);
-  IconStore.set('CopyOutlined', CopyOutlined);
-  IconStore.set('SnippetsOutlined', SnippetsOutlined);
-};
+import { registerIcon } from './utils';
 
 export const useToolbarConfig = createToolbarConfig<FlowchartProps['toolbarPanelProps']>((toolbarConfig, proxy) => {
   const { flowchartId } = proxy.getValue();
