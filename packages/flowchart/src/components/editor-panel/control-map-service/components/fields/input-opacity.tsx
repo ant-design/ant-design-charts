cf93aa1bccc878b47c6986bbb575e72bc19213b5
123beyond <2685124275@qq.com>
2022-02-08 17:15:40 +0800

    新增文字透明度

---
+++ packages/flowchart/src/components/editor-panel/control-map-service/components/fields/input-opacity.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { InputNumber } from 'antd';
+import { FormItemHeight } from '../constants';
+
+interface IProps {
+  label?: string;
+  value?: number;
+  max?: number;
+  min?: number;
+  width?: number;
+  step?: number;
+  onChange?: (value: number) => void;
+  formatter: (value: number) => string;
+}
+const InputOpacity: React.FC<IProps> = (props) => {
+  const { label, value = 100, onChange, max, min, width, step = 10 } = props;
+
+  return (
+    <div className="group">
+      {label && <label>{label}</label>}
+      <InputNumber
+        value={value}
+        max={max}
+        min={min}
+        step={step}
+        style={{
+          width,
+          height: FormItemHeight,
+        }}
+        formatter={(value) => `${value}%`}
+        onChange={(value: number) => {
+          onChange?.(value);
+        }}
+      />
+    </div>
+  );
+};
+export default InputOpacity;
