dd333489a4fb08ffd312396fe141d02bb1ab1b96
Joel Alan <31396322+lxfu1@users.noreply.github.com>
2022-09-16 18:06:43 +0800

    fix: 修改官网示例错误 (#1578)

--- packages/graphs/src/utils/get-children-data.ts
+++ packages/graphs/src/utils/get-children-data.ts
@@ -4,7 +4,7 @@ import { Datum } from '../interface';
  * get children
  * 获取相关路径下的一级节点
  */
-export const getChildrenData = (data: Datum, currentPath: string): Datum => {
+export const getChildrenData = (data: Datum, currentPath: string = ''): Datum => {
   // 打标时已经做了编码，这直接取值即可
   const path = currentPath.split('-');
   path.shift(); // 根节点没有 path
