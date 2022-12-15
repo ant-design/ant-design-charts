e7b2de4e3a943d7da884f8e523834f8817fb12b9
Joel Alan <31396322+lxfu1@users.noreply.github.com>
2022-12-15 14:39:47 +0800

    fix: test live (#1729)
    * fix: test live
    
    * fix: test
    
    * feat: file cache

--- config/webpack.js
+++ config/webpack.js
@@ -130,16 +130,16 @@ const getWebpackConfig = (name, library) => {
       hints: false,
     },
     devtool: 'source-map',
-    // cache: {
-    //   type: 'filesystem',
-    //   buildDependencies: {
-    //     config: [__filename],
-    //   },
-    // },
-    // optimization: {
-    //   runtimeChunk: 'single',
-    //   moduleIds: 'deterministic',
-    // },
+    cache: {
+      type: 'filesystem',
+      buildDependencies: {
+        config: [__filename],
+      },
+    },
+    optimization: {
+      runtimeChunk: 'single',
+      moduleIds: 'deterministic',
+    },
   };
 };
 
--- packages/graphs/jest.config.js
+++ packages/graphs/jest.config.js
@@ -1,2 +1,7 @@
 const { BaseJestConfig } = require('../../config/jest');
-module.exports = BaseJestConfig;
+module.exports = {
+  ...BaseJestConfig,
+  moduleNameMapper: {
+    'd3-((?!linear)\\S*)': `<rootDir>/../../node_modules/d3-$1/dist/d3-$1.min.js`,
+  },
+};
