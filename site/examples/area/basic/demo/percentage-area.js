88ad217ffd9b71bab3315923d2e4eace6a40b20d
chenkun <ck01107787@antgroup.com>
2023-08-15 09:41:12 +0800

    chore: 添加面积图示例

---
+++ site/examples/area/basic/demo/percentage-area.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Area } from '@ant-design/plots';
+
+const DemoArea = () => {
+  const config = {
+    data: {
+      type: 'fetch',
+      value: 'https://assets.antv.antgroup.com/g2/unemployment-by-industry.json',
+    },
+    xField: (d) => new Date(d.date),
+    yField: 'unemployed',
+    colorField: 'industry',
+    normalize: true,
+    stack: true,
+    tooltip: { channel: 'y0', valueFormatter: '.3f' },
+  };
+  return <Area {...config} />;
+};
+
+ReactDOM.render(<DemoArea />, document.getElementById('container'));
