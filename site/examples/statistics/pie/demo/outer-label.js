a357c1c2062846fa0f541ca3c54850dae68a3b49
ancyloce <ancyloce@gmail.com>
2023-08-25 16:45:54 +0800

    refactor: Updated the legend display for the pie chart.

--- site/examples/statistics/pie/demo/outer-label.js
+++ site/examples/statistics/pie/demo/outer-label.js
@@ -1,32 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Pie } from '@ant-design/plots';
+import {Pie} from '@ant-design/plots';
 
 const DemoPie = () => {
   const config = {
     data: [
-      { type: '分类一', value: 27 },
-      { type: '分类二', value: 25 },
-      { type: '分类三', value: 18 },
-      { type: '分类四', value: 15 },
-      { type: '分类五', value: 10 },
-      { type: '其他', value: 5 },
+      {type: '分类一', value: 27},
+      {type: '分类二', value: 25},
+      {type: '分类三', value: 18},
+      {type: '分类四', value: 15},
+      {type: '分类五', value: 10},
+      {type: '其他', value: 5},
     ],
-    appendPadding: 10,
     angleField: 'value',
     colorField: 'type',
-    radius: 0.8,
+    paddingRight: 80,
     label: {
       text: 'value',
       position: 'outside',
     },
     legend: {
       color: {
-        position: "right"
+        title: false,
+        position: "right",
+        rowPadding: 5
       }
     },
   };
   return <Pie {...config} />;
 };
 
-ReactDOM.render(<DemoPie />, document.getElementById('container'));
+ReactDOM.render(<DemoPie/>, document.getElementById('container'));
--- site/examples/statistics/pie/demo/spider-label.js
+++ site/examples/statistics/pie/demo/spider-label.js
@@ -1,33 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Pie } from '@ant-design/plots';
+import {Pie} from '@ant-design/plots';
 
 const data = [
-  { type: '分类一', value: 27 },
-  { type: '分类二', value: 25 },
-  { type: '分类三', value: 18 },
-  { type: '分类四', value: 15 },
-  { type: '分类五', value: 10 },
-  { type: '其他', value: 5 },
+  {type: '分类一', value: 27},
+  {type: '分类二', value: 25},
+  {type: '分类三', value: 18},
+  {type: '分类四', value: 15},
+  {type: '分类五', value: 10},
+  {type: '其他', value: 5},
 ];
 const DemoPie = () => {
   const config = {
     data,
-    appendPadding: 10,
     angleField: 'value',
     colorField: 'type',
-    radius: 0.75,
+    inset: 5,
+    paddingRight: 80,
     label: {
       text: (d) => `${d.type}\n ${d.value}`,
       position: 'spider',
+      transform: [
+        {
+          type: 'overlapDodgeY',
+        },
+      ],
     },
     legend: {
       color: {
-        position: "right"
+        title: false,
+        position: "right",
+        rowPadding: 5
       }
     },
   };
   return <Pie {...config} />;
 };
 
-ReactDOM.render(<DemoPie />, document.getElementById('container'));
+ReactDOM.render(<DemoPie/>, document.getElementById('container'));
