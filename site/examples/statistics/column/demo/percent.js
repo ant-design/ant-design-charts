4ce8bdefefd2e07f0e8d77035b893871617c5597
Joel Alan <31396322+lxfu1@users.noreply.github.com>
2023-09-11 21:29:08 +0800

    chore: prefect percent demos (#2113)

---
+++ site/examples/statistics/column/demo/percent.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Column } from '@ant-design/plots';
+
+const data = [
+  {
+    country: 'Asia',
+    year: '1750',
+    value: 502,
+  },
+  {
+    country: 'Asia',
+    year: '1800',
+    value: 635,
+  },
+  {
+    country: 'Asia',
+    year: '1850',
+    value: 809,
+  },
+  {
+    country: 'Asia',
+    year: '1900',
+    value: 947,
+  },
+  {
+    country: 'Asia',
+    year: '1950',
+    value: 1402,
+  },
+  {
+    country: 'Asia',
+    year: '1999',
+    value: 3634,
+  },
+  {
+    country: 'Asia',
+    year: '2050',
+    value: 5268,
+  },
+  {
+    country: 'Africa',
+    year: '1750',
+    value: 106,
+  },
+  {
+    country: 'Africa',
+    year: '1800',
+    value: 107,
+  },
+  {
+    country: 'Africa',
+    year: '1850',
+    value: 111,
+  },
+  {
+    country: 'Africa',
+    year: '1900',
+    value: 133,
+  },
+  {
+    country: 'Africa',
+    year: '1950',
+    value: 221,
+  },
+  {
+    country: 'Africa',
+    year: '1999',
+    value: 767,
+  },
+  {
+    country: 'Africa',
+    year: '2050',
+    value: 1766,
+  },
+  {
+    country: 'Europe',
+    year: '1750',
+    value: 163,
+  },
+  {
+    country: 'Europe',
+    year: '1800',
+    value: 203,
+  },
+  {
+    country: 'Europe',
+    year: '1850',
+    value: 276,
+  },
+  {
+    country: 'Europe',
+    year: '1900',
+    value: 408,
+  },
+  {
+    country: 'Europe',
+    year: '1950',
+    value: 547,
+  },
+  {
+    country: 'Europe',
+    year: '1999',
+    value: 729,
+  },
+  {
+    country: 'Europe',
+    year: '2050',
+    value: 628,
+  },
+];
+
+
+const DemoArea = () => {
+  const config = {
+    data,
+    xField: 'year',
+    yField: 'value',
+    colorField: 'country',
+    percent: true,
+    stack: true,
+    interaction: {
+      tooltip: {
+        shared: true
+      }
+    },
+    tooltip: { channel: 'y0', valueFormatter: '.0%' },
+  };
+  return <Column {...config} />;
+};
+
+ReactDOM.render(<DemoArea />, document.getElementById('container'));
