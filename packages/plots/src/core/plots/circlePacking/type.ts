85f9e716ad45de2ff7933cd5fd5667ff1788ff93
xifandong.dxf <xifandong.dxf@digital-engine.com>
2023-11-18 16:49:08 +0800

    fix：删除已内置的baseOptions

--- packages/plots/src/core/plots/circlePacking/type.ts
+++ packages/plots/src/core/plots/circlePacking/type.ts
@@ -1,3 +1,3 @@
-import type { BaseOptions, Options } from '../../types/common';
+import type { Options } from '../../types/common';
 
-export type CirclePackingOptions = Options & BaseOptions;
+export type CirclePackingOptions = Options;
