8e433007a29f1ed921b8bc5e6e5beed9fd5715d1
折木 <1076849402@qq.com>
2023-11-20 18:40:57 +0800

    fix: type fix

--- packages/plots/src/core/plots/stock/type.ts
+++ packages/plots/src/core/plots/stock/type.ts
@@ -1,22 +1,17 @@
-import type { BaseOptions, Options } from '../../types/common';
+import type { Options } from '../../types/common';
 
-export type StockOptions = Options &
-  BaseOptions & {
-    /**
-     * @title x 轴字段 日期
-     */
-    readonly xField: string;
-    /**
-     * @title y 轴映射
-     * @description   range  【开盘价/收盘价/最高价/最低价】，设置一个指定 [open, close, high, low]【开盘价/收盘价/最高价/最低价】字段的数组
-     */
-    readonly yField: [string, string, string, string];
-    /**
-     * @title 上涨色
-     */
-    readonly risingFill?: string;
-    /**
-     * @title 下跌色
-     */
-    readonly fallingFill?: string;
-  };
+export type StockOptions = Options & {
+  /**
+   * @title y 轴映射
+   * @description   range  【开盘价/收盘价/最高价/最低价】，设置一个指定 [open, close, high, low]【开盘价/收盘价/最高价/最低价】字段的数组
+   */
+  yField: [string, string, string, string];
+  /**
+   * @title 上涨色
+   */
+  readonly risingFill?: string;
+  /**
+   * @title 下跌色
+   */
+  readonly fallingFill?: string;
+};
