e3250d7396b20c1e466303e93f2e0a7487df2616
yelinlei.yll <yelinlei.yll@digital-engine.com>
2023-11-20 18:02:48 +0800

    feat: 添加 venn 图类型

--- packages/plots/src/core/plots/venn/type.ts
+++ packages/plots/src/core/plots/venn/type.ts
@@ -1,6 +1,16 @@
 import type { BaseOptions, Options } from '../../types/common';
 
-export type VennOptions = Options & BaseOptions;
+export type VennOptions = Options &
+  BaseOptions & {
+    /**
+     * @title 集合空间键名
+     */
+    setsField: string;
+    /**
+     * @title 集合大小键名
+     */
+    sizeField: string;
+  };
 
 export enum DefaultTransformKey {
   color = 'key',
