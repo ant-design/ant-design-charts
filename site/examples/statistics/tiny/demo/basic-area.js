1ceba1b10c305b718d6266555521d7a78a0e7410
Joel Alan <31396322+lxfu1@users.noreply.github.com>
2023-11-21 20:48:11 +0800

    fix: tiny examples (#2210)
    * fix: tiny examples
    
    * fix: site title
    
    * fix: version

--- site/examples/statistics/tiny/demo/basic-area.js
+++ site/examples/statistics/tiny/demo/basic-area.js
@@ -3,25 +3,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const DemoArea = () => {
+  const data = [
+    264, 417, 438, 887, 309, 397, 550, 575, 563, 430, 525, 592, 492, 467, 513, 546, 983, 340, 539, 243, 226, 192,
+  ].map((value, index) => ({ value, index }));
   const config = {
-    data: [
-      264, 417, 438, 887, 309, 397, 550, 575, 563, 430, 525, 592, 492, 467, 513, 546, 983, 340, 539, 243, 226, 192,
-    ],
+    data,
     width: 480,
     height: 80,
     padding: 8,
     shapeField: 'smooth',
-    xField: (_, idx) => idx,
-    yField: (d) => d,
-    scale: {
-      y: { zero: true },
-    },
+    xField: 'index',
+    yField: 'value',
     style: {
       fill: 'linear-gradient(-90deg, white 0%, darkgreen 100%)',
       fillOpacity: 0.6,
     },
-    animate: { enter: { type: 'fadeIn' } },
-    interaction: { tooltip: { render: (e, { title, items }) => items[0].value } },
   };
   return <Tiny.Area {...config} />;
 };
