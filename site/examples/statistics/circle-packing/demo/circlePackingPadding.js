3069c03379ab7f83020f14d7107cb706e3236475
花海 <43226029+dongxifan@users.noreply.github.com>
2023-11-28 14:04:26 +0800

    feat:新增自定义Padding距离类型捆绑图 (#2243)

---
+++ site/examples/statistics/circle-packing/demo/circlePackingPadding.js
@@ -0,0 +1,39 @@
+
+
+import { CirclePacking } from '@ant-design/plots';
+import { interpolateHcl } from 'd3-interpolate';
+import React from 'react';
+import ReactDOM from 'react-dom';
+const DemoCirclePackingPadding = () => {
+
+  const config = {
+    data: {
+      type: "fetch",
+      value: "https://assets.antv.antgroup.com/g2/flare.json",
+    },
+    valueField: "value",
+    colorField: "depth",
+    scale: {
+      color: {
+        domain: [0, 5],
+        range: ["hsl(152,80%,80%)", "hsl(228,30%,40%)"],
+        interpolate: interpolateHcl
+      },
+    },
+    layout: {
+      padding: 5,
+    },
+    label: {
+      text: (d) => d.height === 0 ? d.data.name : '',
+      position: 'inside',
+      transform: [
+        {
+          type: 'overflowHide'
+        },
+      ]
+    }
+  };
+  return <CirclePacking {...config} />;
+};
+
+ReactDOM.render(<DemoCirclePackingPadding />, document.getElementById('container'));
