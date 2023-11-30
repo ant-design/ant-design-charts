39fd91e7005e2535ca297fb8b32af2edb43cf182
Joel Alan <31396322+lxfu1@users.noreply.github.com>
2023-11-30 11:30:53 +0800

    docs: update group and stack example (#2258)

--- site/examples/statistics/column/demo/group-and-stack.js
+++ site/examples/statistics/column/demo/group-and-stack.js
@@ -16,6 +16,45 @@ const DemoColumn = () => {
       series: false,
     },
     colorField: 'product_sub_type',
+    tooltip: (item) => {
+      return { origin: item };
+    },
+    interaction: {
+      tooltip: {
+        render: (e, { title, items }) => {
+          return (
+            <div>
+              <h4>{title}</h4>
+              {items.map((item) => {
+                const { name, color, origin } = item;
+                return (
+                  <div>
+                    <div style={{ margin: 0, display: 'flex', justifyContent: 'space-between' }}>
+                      <div>
+                        <span
+                          style={{
+                            display: 'inline-block',
+                            width: 6,
+                            height: 6,
+                            borderRadius: '50%',
+                            backgroundColor: color,
+                            marginRight: 6,
+                          }}
+                        ></span>
+                        <span>
+                          {origin['product_sub_type']}-{name}
+                        </span>
+                      </div>
+                      <b>{origin['order_amt']}</b>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        },
+      },
+    },
   };
   return <Column {...config} />;
 };
