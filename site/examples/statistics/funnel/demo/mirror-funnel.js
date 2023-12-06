075f1a07a72b69bb6b053897c8fa0d4a07df4135
lxfu1 <954055752@qq.com>
2023-12-06 15:38:44 +0800

    fix: create node

--- site/examples/statistics/funnel/demo/mirror-funnel.js
+++ site/examples/statistics/funnel/demo/mirror-funnel.js
@@ -16,6 +16,13 @@ const DemoFunnel = () => {
     { action: '完成', visitor: 80, site: '站点2' },
   ];
 
+  const uPosition = (item, values) => {
+    if (item.site === '站点2') {
+      return values[0];
+    }
+    return values[1];
+  };
+
   const config = {
     data,
     xField: 'action',
@@ -24,9 +31,39 @@ const DemoFunnel = () => {
     style: {
       stroke: '#fff',
     },
-    label: {
-      text: 'visitor',
-    },
+    label: [
+      {
+        text: (d) => d.visitor,
+        position: 'inside',
+        fontSize: 16,
+      },
+      {
+        render: ($, _, i) => {
+          if (i)
+            return (
+              <div
+                key={i}
+                style={{
+                  height: 1,
+                  width: 30,
+                  background: '#aaa',
+                  marginLeft: _.site === '站点2' ? -30 : 0,
+                }}
+              ></div>
+            );
+        },
+        position: (item) => uPosition(item, ['top-left', 'top-right']),
+      },
+      {
+        text: (d, i, data) => {
+          if (i) return ((d.visitor / data[i - 1].visitor) * 100).toFixed(2) + '%';
+        },
+        position: (item) => uPosition(item, ['top-left', 'top-right']),
+        textAlign: (item) => uPosition(item, ['right', 'left']),
+        textBaseline: 'middle',
+        dx: (item) => uPosition(item, [-40, 40]),
+      },
+    ],
     legend: false,
   };
   return <Funnel {...config} />;
