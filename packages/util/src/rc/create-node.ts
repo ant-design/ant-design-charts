14ce87bc279b90a957defb35b8084fd556bb7c74
Joel Alan <31396322+lxfu1@users.noreply.github.com>
2023-12-06 16:16:22 +0800

    fix: create node (#2280)

--- packages/util/src/rc/create-node.ts
+++ packages/util/src/rc/create-node.ts
@@ -4,7 +4,7 @@ import { render } from '../react/render';
 const mountMapping = new Map();
 export const createNode = (children: React.ReactElement) => {
   let mount = document.createElement('div');
-  if (children.key) {
+  if (children?.key) {
     if (!mountMapping.get(children.key)) {
       mountMapping.set(children.key, mount);
     }
