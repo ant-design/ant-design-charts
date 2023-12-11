324dccb62adc50e3ee825065e35cdafb9e483fbb
Frank William <65594180+ai-qing-hai@users.noreply.github.com>
2023-12-11 20:38:57 +0800

    fix(plot): 修复 bullet update layout 后坐标不生效的问题 (#2289)
    * fix(plot): 修复 bullet update layout 后坐标不生效的问题
    
    * fix(plot): 修复 bullet update layout 后坐标不生效的问题
    
    ---------
    
    Co-authored-by: wb-xcf804241 <wb-xcf804241@wb-xcf804241deMacBook-Pro.local>

--- packages/plots/src/core/components/index.ts
+++ packages/plots/src/core/components/index.ts
@@ -1,3 +1,3 @@
 export { mark } from './mark';
 export { coordinate, COORDIANTE_OPTIONS } from './coordinate';
-export { coordinateLayout } from './coordinateLayout';
+export * from './coordinate-layout';
