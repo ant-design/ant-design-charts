2b390bb721b40b733dea47d45359a3ffab9431ff
Joel Alan <31396322+lxfu1@users.noreply.github.com>
2023-12-15 21:36:26 +0800

    refactor: annotations (#2302)

--- packages/plots/src/core/utils/delete-excess-keys.ts
+++ packages/plots/src/core/utils/delete-excess-keys.ts
@@ -1,4 +1,4 @@
-import { TRANSFORM_OPTION_KEY, VIEW_OPTIONS, CONFIG_SHAPE, COORDIANTE_OPTIONS } from '../constants';
+import { TRANSFORM_OPTION_KEY, VIEW_OPTIONS, CONFIG_SHAPE, COORDIANTE_OPTIONS, ANNOTATION_LIST } from '../constants';
 import { Options } from '../types';
 
 /**
@@ -21,9 +21,9 @@ export const deleteExcessKeys = <O extends Options>(options: O): O => {
       }
     });
   });
-  /** 删除不在 View 里面配置，避免多次 Transform & Scale 等 */
+  /** 删除不在 View 和自定义 Annotations 里面配置，避免多次 Transform & Scale 等 */
   Object.keys(options).forEach((key) => {
-    if (!VIEW_OPTIONS.includes(key)) {
+    if (![...VIEW_OPTIONS, ...ANNOTATION_LIST.map((item) => item.key)].includes(key)) {
       delete options[key];
     }
   });
