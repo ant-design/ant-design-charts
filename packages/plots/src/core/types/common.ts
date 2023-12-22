aeb06fcb996068bf013eef719a510654c938025e
Joel Alan <31396322+lxfu1@users.noreply.github.com>
2023-12-22 10:15:26 +0800

    fix: connect null type (#2307)

--- packages/plots/src/core/types/common.ts
+++ packages/plots/src/core/types/common.ts
@@ -100,3 +100,18 @@ export type Adaptor<P = Options> = {
 };
 
 export type AttrStyle = Record<string, any>;
+
+export type ConnectNulls =
+  | true
+  | {
+      connect: true;
+      /**
+       * @title 连线样式
+       * @description 和 canvas path 一致，但需要加上 connect 前缀
+       * @example
+       *  - connectStroke: '#ccc'
+       *  - connectLineWidth: 1
+       *  - connectLineDash: [ 4, 4 ]
+       */
+      [key: string]: any;
+    };
