bbb6fa92d724f26b23fa1b87fc65a9d4a06b1105
lxfu1 <954055752@qq.com>
2024-01-16 22:32:28 +0800

    fix: react element

--- packages/plots/src/util/is-valid-element.ts
+++ packages/plots/src/util/is-valid-element.ts
@@ -1,5 +1,5 @@
 export const isValidElement = (jsxCode: string): boolean => {
-  const jsxRegex = /react|\.jsx|children:\[\(/i;
+  const jsxRegex = /react|\.jsx|children:\[\(|return\s+\S+.createElement\([^\)]*,/i;
 
   return jsxRegex.test(jsxCode);
 };
