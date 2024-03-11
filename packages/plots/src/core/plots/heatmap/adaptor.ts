9b87a6d8c1cc246b7b4ee0d10cdb27cf32b27d65
wb-xcf804241 <wb-xcf804241@alibaba-inc.com>
2024-03-11 15:37:26 +0800

    feat(plot): add heatmap demo

--- packages/plots/src/core/plots/heatmap/adaptor.ts
+++ packages/plots/src/core/plots/heatmap/adaptor.ts
@@ -15,7 +15,7 @@ export function adaptor(params: Params) {
    */
   const tooltip = (params: Params) => {
     const { options } = params;
-    const { tooltip, colorField, sizeField } = options;
+    const { tooltip = {}, colorField, sizeField } = options;
     if (!tooltip.field) {
       tooltip.field = colorField || sizeField;
     }
