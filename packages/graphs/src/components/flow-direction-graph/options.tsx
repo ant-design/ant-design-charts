d663a5d0d1f0bdb7d155533d1d39b02554bd1a7f
yvonneyx <zhuyuxin0627@gmail.com>
2024-12-11 14:22:29 +0800

    feat: support labelField in FlowDirectionGraph and FlowGraph

--- packages/graphs/src/components/flow-direction-graph/options.tsx
+++ packages/graphs/src/components/flow-direction-graph/options.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { RCNode } from '../../core/base';
+import { formatLabel } from '../../core/utils/label';
 import type { GraphOptions } from '../../types';
+import type { FlowDirectionGraphOptions } from './types';
 
 const { TextNode } = RCNode;
 
 export const DEFAULT_OPTIONS: GraphOptions = {
   node: {
     type: 'react',
-    style: {
-      component: (data) => <TextNode type="filled" text={data.id} />,
-      size: [100, 40],
-      ports: [{ placement: 'left' }, { placement: 'right' }],
-    },
     state: {
       active: {
         halo: false,
@@ -38,4 +35,23 @@ export const DEFAULT_OPTIONS: GraphOptions = {
     animation: false,
   },
   transforms: ['translate-react-node-origin'],
+};
+
+export const getFlowDirectionGraphOptions = ({
+  labelField,
+}: Pick<FlowDirectionGraphOptions, 'labelField'>): FlowDirectionGraphOptions => {
+  const options: FlowDirectionGraphOptions = {
+    node: {
+      style: {
+        component: (data) => {
+          const label = formatLabel(data, labelField);
+          return <TextNode type="filled" text={label} />;
+        },
+        size: [100, 40],
+        ports: [{ placement: 'left' }, { placement: 'right' }],
+      },
+    },
+  };
+
+  return options;
 };
