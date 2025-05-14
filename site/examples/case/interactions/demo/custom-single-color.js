ce7efa75bc60b2e9ed06de98c79e8c4c03cd811d
Joel Alan <31396322+lxfu1@users.noreply.github.com>
2025-05-14 10:06:27 +0800

    chore: add custom color examples (#2906)

---
+++ site/examples/case/interactions/demo/custom-single-color.js
@@ -0,0 +1,55 @@
+import { Column } from '@ant-design/plots';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const DemoCustomSingleColor = () => {
+  const config = {
+    data: [
+      {
+        type: '家具家电',
+        sales: 38,
+      },
+      {
+        type: '粮油副食',
+        sales: 52,
+      },
+      {
+        type: '生鲜水果',
+        sales: 61,
+      },
+      {
+        type: '美容洗护',
+        sales: 145,
+      },
+      {
+        type: '母婴用品',
+        sales: 48,
+      },
+      {
+        type: '进口食品',
+        sales: 38,
+      },
+      {
+        type: '食品饮料',
+        sales: 8,
+      },
+      {
+        type: '家庭清洁',
+        sales: 28,
+      },
+    ],
+    xField: 'type',
+    yField: 'sales',
+    style: {
+      fill: ({ type }) => {
+        if (type === '美容洗护') {
+          return '#f4664a';
+        }
+        return '#1890ff';
+      },
+    },
+  };
+  return <Column {...config} />;
+};
+
+ReactDOM.render(<DemoCustomSingleColor />, document.getElementById('container'));
