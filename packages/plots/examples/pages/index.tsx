091c3b080b0f9355b41dd25ac6b49454326d57df
lxfu1 <954055752@qq.com>
2025-12-09 17:58:25 +0800

    fix: bullet legend invalid configuration

--- packages/plots/examples/pages/index.tsx
+++ packages/plots/examples/pages/index.tsx
@@ -6,6 +6,10 @@ const dataSource = [
     example: 'radar',
     description: '雷达图所有值为 0 的场景',
   },
+  {
+    example: 'bullet-legend',
+    description: '子弹图关闭图例',
+  },
 ].map((item, index) => ({ key: index, ...item }));
 
 export const ExampleList = () => {
@@ -16,13 +20,13 @@ export const ExampleList = () => {
         margin: '24px auto',
         border: '1px solid #eee',
         borderRadius: 4,
-        padding: 24,
+        padding: 12,
         boxShadow: '0 4px 12px rgba(0,0,0,0.1)',
       }}
     >
       {dataSource.map((data) => (
-        <div key={data.key}>
-          <Link to={`/${data.example}`} target="_blank" rel="noreferrer">
+        <div key={data.key} style={{ margin: 12 }}>
+          <Link to={`/${data.example}`} target="_blank" rel="noreferrer" title={data.description}>
             <b>{data.example}: </b>
             <span>{data.description}</span>
           </Link>
