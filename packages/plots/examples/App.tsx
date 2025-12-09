141e0b789e9fcf852d49145f337d1011e45a55dc
lxfu1 <954055752@qq.com>
2025-12-09 18:04:38 +0800

    fix: bullet legend invalid cofiguration

--- packages/plots/examples/App.tsx
+++ packages/plots/examples/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { ExampleList } from './pages';
+import { DemoBulletLegend } from './pages/bullet-legend';
 import { DemoRadar } from './pages/radar';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<ExampleList />} />
       <Route path="/radar" element={<DemoRadar />} />
+      <Route path="/bullet-legend" element={<DemoBulletLegend />} />
     </Routes>
   );
 }
